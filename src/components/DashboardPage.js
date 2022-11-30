4fdf81d9d15f6b5c1145a64b3ee26fdd50d6ac86
Hamid Farmani <hamidfarmani1@gmail.com>
2022-11-30 13:53:42 +0100

    docs: explanation for each algorithm added

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,6 +1,16 @@
-import { Paper, ScrollArea, Space, Text, Title } from "@mantine/core";
+import {
+  Accordion,
+  Paper,
+  ScrollArea,
+  Space,
+  Text,
+  Title,
+} from "@mantine/core";
+import { useState } from "react";
 
 const DashboardPage = () => {
+  const [value, setValue] = useState([]);
+
   return (
     <ScrollArea style={{ height: 730 }}>
       <Paper shadow="md" p="md">
@@ -42,29 +52,129 @@ const DashboardPage = () => {
 
         <Space h="xl" />
 
-        <Title order={2}>Merge sort</Title>
-        <Space h="sm" />
+        <Accordion
+          value={value}
+          onChange={setValue}
+          variant="separated"
+          defaultValue="merge-sort"
+        >
+          <Accordion.Item value="merge-sort">
+            <Accordion.Control>Merge sort</Accordion.Control>
+            <Accordion.Panel>
+              <Text>
+                The Merge Sort algorithm is a sorting algorithm that is based on
+                the Divide and Conquer paradigm. In this algorithm, the array is
+                initially divided into two equal halves and then they are
+                combined in a sorted manner.
+              </Text>
+              <Space h="sm" />
 
-        <Text>
-          The Merge Sort algorithm is a sorting algorithm that is based on the
-          Divide and Conquer paradigm. In this algorithm, the array is initially
-          divided into two equal halves and then they are combined in a sorted
-          manner.
-        </Text>
-        <Space h="sm" />
+              <Title order={4}>Merge Sort Working Process</Title>
+              <Text>
+                Think of it as a recursive algorithm continuously splits the
+                array in half until it cannot be further divided. This means
+                that if the array becomes empty or has only one element left,
+                the dividing will stop, i.e. it is the base case to stop the
+                recursion. If the array has multiple elements, split the array
+                into halves and recursively invoke the merge sort on each of the
+                halves. Finally, when both halves are sorted, the merge
+                operation is applied. Merge operation is the process of taking
+                two smaller sorted arrays and combining them to eventually make
+                a larger one.
+              </Text>
+            </Accordion.Panel>
+          </Accordion.Item>
 
-        <Title order={4}>Merge Sort Working Process</Title>
-        <Text>
-          Think of it as a recursive algorithm continuously splits the array in
-          half until it cannot be further divided. This means that if the array
-          becomes empty or has only one element left, the dividing will stop,
-          i.e. it is the base case to stop the recursion. If the array has
-          multiple elements, split the array into halves and recursively invoke
-          the merge sort on each of the halves. Finally, when both halves are
-          sorted, the merge operation is applied. Merge operation is the process
-          of taking two smaller sorted arrays and combining them to eventually
-          make a larger one.
-        </Text>
+          <Accordion.Item value="bubble-sort">
+            <Accordion.Control>Bubble sort</Accordion.Control>
+            <Accordion.Panel>
+              <Text>
+                Bubble Sort is a straighthood, easy to understand sorting
+                algorithm. It works by looping through an array and comparing
+                neighbouring elements, then swapping them if they are in the
+                wrong order. In this fashion, the largest number “bubbles” to
+                the top. This is repeated until the array is sorted.
+              </Text>
+
+              <Space h="sm" />
+
+              <Title order={4}>Bubble Sort Working Process</Title>
+              <Text>
+                1. Start at the beginning of the array.
+                <br />
+                2. Is the adjacent element to the right less? If so, swap.
+                <br />
+                3. Move up to next element.
+                <br />
+                4. Repeat steps 2-3 until array is sorted.
+              </Text>
+            </Accordion.Panel>
+          </Accordion.Item>
+
+          <Accordion.Item value="insertion-sort">
+            <Accordion.Control>Insertion sort</Accordion.Control>
+            <Accordion.Panel>
+              <Text>
+                Insertion sort is a simple sorting algorithm that works similar
+                to the way you sort playing cards in your hands. The array is
+                virtually split into a sorted and an unsorted part. Values from
+                the unsorted part are picked and placed at the correct position
+                in the sorted part.
+              </Text>
+
+              <Space h="sm" />
+
+              <Title order={4}>Insertion Sort Working Process</Title>
+              <Text>
+                1. Begin with a list of unsorted elements.
+                <br />
+                2. Iterate through the list of unsorted elements, from the first
+                item to last.
+                <br />
+                3. The current element is compared to the elements in all
+                preceding positions to the left in each step.
+                <br />
+                4. If the current element is less than any of the previously
+                listed elements, it is moved one position to the left.
+              </Text>
+            </Accordion.Panel>
+          </Accordion.Item>
+
+          <Accordion.Item value="selection-sort">
+            <Accordion.Control>Selection sort</Accordion.Control>
+            <Accordion.Panel>
+              <Text>
+                The selection sort algorithm sorts an array by repeatedly
+                finding the minimum element (considering ascending order) from
+                the unsorted part and putting it at the beginning. The algorithm
+                maintains two subarrays in a given array.
+                <br />
+                - The subarray which already sorted.
+                <br />
+                - The remaining subarray was unsorted.
+                <br />
+                In every iteration of the selection sort, the minimum element
+                (considering ascending order) from the unsorted subarray is
+                picked and moved to the sorted subarray.
+              </Text>
+
+              <Space h="sm" />
+
+              <Title order={4}>Selection Sort Working Process</Title>
+              <Text>
+                1. Set min to the first location.
+                <br />
+                2. Search the minimum element in the array.
+                <br />
+                3. swap the first location with the minimum value in the array.
+                <br />
+                4. assign the second element as min.
+                <br />
+                5. Repeat the process until we get a sorted array.
+              </Text>
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
 
         <Space h="xl" />
       </Paper>
